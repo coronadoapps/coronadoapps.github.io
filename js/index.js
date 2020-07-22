0968cff0d004aaec2fff84345f67af21e741ee2c
stiven c <iamalescoronado@gmail.com>
2020-07-22 17:56:34 -0500

    cambio de color ball

--- js/index.js
+++ js/index.js
@@ -5,13 +5,20 @@ var x = canvas.width/2;
 var y = canvas.height/2;
 var dx = 2;
 var dy = -2;
+var ballColor = changeColor();
 
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#ffd700";
+    ctx.fillStyle = ballColor;
     ctx.fill();
     ctx.closePath();
+}
+
+function changeColor(){
+    var color = '#';    
+    color += Math.floor(Math.random()*16777215).toString(16);
+      return color;
 }
 
 function draw() {
@@ -20,9 +27,11 @@ function draw() {
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
+        ballColor = changeColor();
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
+        ballColor = changeColor();
     }
     
     x += dx;
