2fbc213f604d14282623bfd23addb4a8bc8cf1fd
stiven c <iamalescoronado@gmail.com>
2020-08-13 17:22:12 -0500

    update 13/8

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 const topbar = document.querySelector('.navbar');
+const buttonTop = document.getElementById('volverTop');
+
 window.addEventListener('scroll', () =>{
     if(window.scrollY > 150){
         topbar.style.backgroundColor = "#222";
+        buttonTop.classList.remove("invisible");
     } else{
         topbar.style.backgroundColor = "transparent";
+        buttonTop.classList.add("invisible");
     }
 });
-const buttonTop = document.querySelector('#volverTop');
 
 function volverTop(){
     window.scrollTo(0, 0); // values are x,y-offset
